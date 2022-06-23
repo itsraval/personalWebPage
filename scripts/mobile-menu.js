5d36bda8fa25cc7ee05699d39e0aae8549af365b
Alessandro Ravizzotti <42083609+itsraval@users.noreply.github.com>
2022-06-24 00:21:32 +0200

    Add files via upload

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -2,6 +2,7 @@ function showMenu(y) {
     y.classList.toggle("change");
     var x = document.getElementsByClassName("menu")[0];
     x.classList.toggle("new");
+    document.body.classList.add('lock-scroll');
 }
 
 function hideMenu(){
@@ -9,4 +10,5 @@ function hideMenu(){
     y.classList.toggle("new");
     var x = document.getElementsByClassName("menu-button")[0];
     x.classList.toggle("change");
+    document.body.classList.remove('lock-scroll');
 }
